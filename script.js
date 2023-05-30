a23dee753f2d7f1a55e21930dbab1e418f17c9ca
Filomn Abrha <filmonabrha2020@gmail.com>
2023-05-30 17:06:35 +0300

    Created the getComputerChoice function

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-Create a getComputerChoice() function that returns Rock, Paper or Scissors.
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters
+// Create a getComputerChoice() function that returns Rock, Paper or Scissors.
+// Write a function that passes the three strings as a possible values.
+let string = ["Rock", "Paper", "Scissors"];
+function getComputerChoice(){
+    return string[Math.floor(Math.random()*string.length)];
+}
+console.log(getComputerChoice());
+// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters
