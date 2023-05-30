9874818c8c0994132eb8ed99725a9d987c6720b7
Filomn Abrha <filmonabrha2020@gmail.com>
2023-05-30 16:28:15 +0300

    Added two algorithms to solve the project

---
+++ script.js
@@ -0,0 +1,2 @@
+Create a getComputerChoice() function that returns Rock, Paper or Scissors.
+Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters
