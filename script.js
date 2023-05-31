afc1cf22d17e92442e2dcbe9fe3d6e808923f41e
Filomn Abrha <filmonabrha2020@gmail.com>
2023-05-31 09:21:32 +0300

    Create a one round Rock-Paper-Scissors game

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-// Create a getComputerChoice() function that returns Rock, Paper or Scissors.
-// Write a function that passes the three strings as a possible values.
+// Create a getcomputerSelection() function that returns Rock, Paper or Scissors.
+// I actualy had to look up how to select from arrays randomly in Stack Overflow.
 let string = ["Rock", "Paper", "Scissors"];
+// console.log(getComputerChoie());
 function getComputerChoice(){
     return string[Math.floor(Math.random()*string.length)];
 }
-console.log(getComputerChoice());
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters
+function playRound(playerSelection,computerSelection) {
+    // if playerSelection is Rock and getCmputerChoice is Paper
+    if (playerSelection==="Rock" && computerSelection==="Paper") {
+        return "You Lose! Paper beats Rock";
+    } 
+    // if playerSelection is Rock and getCmputerChoice is Scissors
+    else if (playerSelection==="Rock" && computerSelection==="Scissors") {
+        return "You Won! Rock beats Scissors";
+    } 
+    // if playerSelection is Paper and getCmputerChoice is Scissors
+    else if (playerSelection==="Paper" && computerSelection==="Scissors") {
+        return "You Lose! Scissors beat Paper";
+    } 
+    // if playerSelection is Paper and getCmputerChoice is Rock
+    else if (playerSelection==="Paper" && computerSelection==="Rock") {
+        return "You Won! Paper beats Rock";
+    } 
+    // if playerSelection is Scissors and getCmputerChoice is Rock
+    else if (playerSelection==="Scissors" && computerSelection==="Rock") {
+        return "You Won! Rock beats Scissors";
+    } 
+    // if playerSelection is Scissors and getCmputerChoice is Paper
+    else if (playerSelection === "Scissors" && computerSelection==="Paper") {
+        return "You Lose! Scissors beat Paper";
+    } else {
+        return "Draw! Not Bad";
+    }
+}
+const playerSelection = "Rock";
+console.log(playerSelection);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection ));
+
