523153eb585f0288b7b0d0b26c562d8c3455c7ce
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-02 09:19:13 +0300

    Make the playerSelection and computerSelection uppercase

--- script.js
+++ script.js
@@ -10,43 +10,54 @@ function getComputerChoice(){
 // if the playerSelection beats the computerSelection
 // if it's a tie. means both selects the same ite
 function playGround(playerSelection, computerSelection){
-    if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Rock")) {
+    // if the playerselection beats the computerSelection
+    if ((playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||
+        (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase()) ||
+        (playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase())) {
             return `You Won! ${playerSelection} beats ${computerSelection}`;
     } 
-    else if  ((playerSelection === "Rock" && computerSelection === "Paper") ||
-              (playerSelection === "Paper" && computerSelection === "Scissors") ||  
-              (playerSelection === "Scissors" && computerSelection === "Rock")) {
+    // if the computerSelection beats the playerSelection
+    else if  ((playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase()) ||
+              (playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||  
+              (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase())) {
            return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (playerSelection===computerSelection) {
+    } 
+    // if both came up with the same selection
+    else if (playerSelection.toUpperCase()===computerSelection.toUpperCase()) {
         return `It's a tie! ${playerSelection} and  ${computerSelection} are the same!` 
     } else {
         return "You entred invalid word!"
     }  
 }
+// create a function called game()
 function game() {
+    // declare two variables to record the score of each round
     let playerScore= 0;
     let computerScore = 0;
+    // using for loop to let the player to play 5 roundes
     for (let round = 1; round <= 5; round++){
-        let computerSelection = getComputerChoice();
-        console.log(`The Computer Selects ${computerSelection}`)
-        let playerSelection = (prompt("Enter your selection: "));
+        // invoke the function and the user input also the computer choice here will
+        // make the loop to ask the user and the computer in each round 
+        let playerSelection = (prompt("Enter your selection: ")).toUpperCase();
         console.log(`You Selected ${playerSelection}`);
+        let computerSelection = getComputerChoice().toUpperCase();
+        console.log(`The Computer Selects ${computerSelection}`)
         let result = playGround(playerSelection, computerSelection);
+        // conditionals on who won the round
         if (result === `You Won! ${playerSelection} beats ${computerSelection}`) {
             console.log(`You Won! ${playerSelection} beats ${computerSelection}`);
             playerScore++;
-            console.log(playerScore, computerScore);
+            console.log(`You:${playerScore}, Computer${computerScore}`);
         } else if(result === `You Lose! ${computerSelection} beats ${playerSelection}`) {
             console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
             computerScore++;  
-            console.log(playerScore, computerScore);
+            console.log(`You:${playerScore}, Computer${computerScore}`);
         } else {
             console.log("It's a tie"); 
-            console.log(playerScore, computerScore);  
+            console.log(`You:${playerScore}, Computer${computerScore}`);  
         }      
     }
+    // condition on deciding who the Final winner is
     if (playerScore > computerScore) {
         console.log(`Wow! You Won ${playerScore} to ${computerScore}!`);
     } else if (playerScore < computerScore){
