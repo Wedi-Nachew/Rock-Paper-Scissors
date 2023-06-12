97c2874b0e31f66313902d8526f84a686acb2f3f
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-12 11:33:15 +0300

    Show the play again text when on player reaches 5

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// a function that randomy selects from the array lists
+const scoreBoard = document.querySelector("#score-board")
+const score = document.querySelector("#score")
+const roundResult = document.querySelector("#round-result")
+const finalResult = document.querySelector("#final-result")
+
+score.classList.add('score')
+roundResult.classList.add('round-result')
+
+
 let options = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
@@ -12,21 +20,20 @@ function getUserSelection(whichOne) {
     if(whichOne == "rock") {
         playerSelection = "rock";
         let result = playGround(playerSelection, computerSelection); 
-        console.log(game())
+        game()
     }  
       else if(whichOne == "paper") {
         playerSelection =  "paper";
         let result = playGround(playerSelection, computerSelection); 
-        console.log(game())
+       game()
       
     } else if (whichOne == "scissors") {
         playerSelection =   "scissors"; 
         let result =  playGround(playerSelection, computerSelection);
-        console.log(game())
+       game()
       
     }
 }
-console.log(getUserSelection())
 function playGround(playerSelection, computerSelection){
     if ((playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||
         (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase()) ||
@@ -44,25 +51,36 @@ function playGround(playerSelection, computerSelection){
 function game() {
     let result = playGround(playerSelection, computerSelection);
         if (result === `You Won! ${playerSelection} beats ${computerSelection}`) {
-            console.log(`You Won! ${playerSelection} beats ${computerSelection}`);
+            roundResult.textContent = `You Won! ${playerSelection} beats ${computerSelection}`;
             playerScore++;
-            console.log(`You: ${playerScore}, Computer: ${computerScore}`);
+            score.textContent = `You: ${playerScore}, Computer: ${computerScore}`;
         } else if(result === `You Lose! ${computerSelection} beats ${playerSelection}`) {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            roundResult.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
             computerScore++;  
-            console.log(`You: ${playerScore}, Computer: ${computerScore}`);
+            score.textContent = `You: ${playerScore}, Computer: ${computerScore}`;
         } else if(playerSelection.toUpperCase()===computerSelection.toUpperCase()){
-            console.log("It's a tie"); 
-            console.log(`You: ${playerScore}, Computer: ${computerScore}`);  
+            roundResult.textContent = ("It's a tie"); 
+            score.textContent =`You: ${playerScore}, Computer: ${computerScore}`;  
         }
 
         if (playerScore == 5 ) {
-            console.log(`Wow! You Won ${playerScore} to ${computerScore}!`);
+            alert(finalResult.textContent = `Wow! You Won ${playerScore} to ${computerScore}!`);
             computerScore = 0;
             playerScore = 0;
+            roundResult.textContent = "Play Again!" 
+            finalResult.textContent = ""
+            score.textContent = ""
         } else if ( computerScore == 5){
-            console.log(`Loser! The Computer Beats you ${playerScore} to ${computerScore}`)
+            alert(finalResult.textContent = `Loser! The Computer Beats you ${playerScore} to ${computerScore}`)
             computerScore = 0;
             playerScore = 0;
+            roundResult.textContent= "Play Again"
+            score.textContent = ""
+            finalResult.textContent = ""
         }     
     } 
+
+
+    scoreBoard.appendChild(roundResult)
+    scoreBoard.appendChild(score)
+    scoreBoard.appendChild(finalResult)
